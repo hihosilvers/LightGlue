7ee46e42f25d45501a0b3b71e62b78e2a0ff3fe1
teejK <onetoast@gmail.com>
2024-08-27 00:18:17 +0100

    update for pytorch depracation

--- lightglue/lightglue.py
+++ lightglue/lightglue.py
@@ -21,7 +21,7 @@ else:
 torch.backends.cudnn.deterministic = True
 
 
-@torch.cuda.amp.custom_fwd(cast_inputs=torch.float32)
+@torch.amp.custom_fwd(cast_inputs=torch.float32, device_type="cuda")
 def normalize_keypoints(
     kpts: torch.Tensor, size: Optional[torch.Tensor] = None
 ) -> torch.Tensor:
@@ -339,7 +339,9 @@ class LightGlue(nn.Module):
     required_data_keys = ["image0", "image1"]
 
     version = "v0.1_arxiv"
-    url = "https://github.com/hihosilvers/LightGlue/releases/download/{}/{}_lightglue.pth"
+    url = (
+        "https://github.com/hihosilvers/LightGlue/releases/download/{}/{}_lightglue.pth"
+    )
 
     features = {
         "superpoint": {
